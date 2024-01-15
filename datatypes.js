66ffffefdc08344b9716c00cbbeb6e73b875551c
bolarinaishat <mohammedolayinka1@gmail.com>
2024-01-15 03:14:54 +0100

    Day1

---
+++ datatypes.js
@@ -0,0 +1,6 @@
+//aanswer to qquetion 5
+console.log(typeof "Nigerial"  )
+console.log(typeof true )
+console.log(typeof  undefine)
+console.log (typeof null)
+
---
+++ variable.js
@@ -0,0 +1,9 @@
+//answer to quetion 4
+let state = "kwara"
+let student = true
+let school 
+let emptyValue = null
+console.log(state, student, school,emptyValue );
+
+
+
